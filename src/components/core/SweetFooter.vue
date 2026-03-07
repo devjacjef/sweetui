6bbb1fc0ccb6827bf18024d94e650524ae30d02a
Jack J <dev.jac.jef@proton.me>
2026-03-07 17:14:43 +0000

    feat(core): add styling to footer

--- src/components/core/SweetFooter.vue
+++ src/components/core/SweetFooter.vue
@@ -1,9 +1,29 @@
-<script setup></script>
+<script setup lang="ts">
+import type { SweetBase } from "@/types/shared";
+import { computed } from "vue";
+
+interface SweetFooter extends SweetBase {}
+
+const props = withDefaults(defineProps<SweetFooter>(), {
+    sweet: true,
+});
+
+const classes = computed(() => ({
+    "sweet-footer": props.sweet,
+}));
+</script>
 
 <template>
-    <footer class="sweet-footer">
+    <footer :class="classes">
         <slot></slot>
     </footer>
 </template>
 
-<style></style>
+<style>
+.sweet-footer {
+    min-width: 100vw;
+    position: relative;
+    bottom: 0;
+    left: 0;
+}
+</style>
