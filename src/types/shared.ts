0e293443a07a101ea1e1a3d6c6bda8fb0cec3da6
Jack J <dev.jac.jef@proton.me>
2026-03-07 16:31:12 +0000

    feature(core): added shared types
    Added shared types for components

---
+++ src/types/shared.ts
@@ -0,0 +1,11 @@
+// Shared type interfaces
+
+/**
+ * Defines props for `sweetui` components.
+ *
+ * @property {boolean} [sweet] - Controls whether sweet styling is applied.
+ * @default true
+ */
+export interface SweetBase {
+  sweet?: boolean;
+}
